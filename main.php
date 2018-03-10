bf5a08848013edeb757b3308ddd293558e6b7ce9
Majid <majid.a.iran@gmail.com>
2018-03-10 23:33:14 +0330

    Update main.php

--- main.php
+++ main.php
@@ -6,7 +6,7 @@ $robot = new RobotClient('https://api.hetzner.cloud/v1', 'PUT_YOUR_ACCESS_TOKEN_
 
 $results = $robot->GetDatacenters('2');
 
-if($results['response_code'] == 201)
+if($results['response_code'] == METHOD_RETURN_CODE)
 {
   print_r($results);
 }
